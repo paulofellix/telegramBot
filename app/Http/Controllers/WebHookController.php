51c06da8c8683f24404a2d1bfb3fb2b7b0e0c2db
Paulo Félix <paulofellix@gmail.com>
2016-11-30 18:23:17 -0400

    Função "/start"

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -15,7 +15,21 @@ class WebHookController extends BaseController
       $fromName = $update['from']['first_name'];
       $chatId = $update['chat']['id'];
       $text = $update['text'];
-      $data = array('chat_id'=>$chatId,'text'=>'Replay: '.$text);
+
+      switch ($text) {
+        case '/start':
+            $msg = 'Oi, eu sou o bot Paulo Félix ;)
+Meu dev master ainda está implementando minhas funções !';
+          break;
+          
+        default:
+            $msg = sprintf('Oi %s, você disse:
+
+<b>%s</b>',$fromName,$text);
+          break;
+      }
+
+      $data = array('chat_id'=>$chatId,'text'=> $msg,'parse_mode'=>'HTML');
       Requests::post('https://api.telegram.org/bot'.$_ENV['BOT_TOKEN'].'/sendMessage',array(),$data);
       return 'ok';
     }
